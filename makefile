52e23b2db32dec83f23cea4a32cb8d18529b761c
jrootham <jrootham@gmail.com>
2015-05-16 13:58:49 -0400

    Early commit

---
+++ makefile
@@ -0,0 +1,4 @@
+global: spiral-package.js
+
+spiral-package.js:
+	browserify spiral.js -o spiral-package.js
---
+++ spiral.js
@@ -0,0 +1,11 @@
+/**
+ * spiral.js
+ *
+ * Created by jrootham on 16/05/15.
+ *
+ * Copyright © 2014 Jim Rootham
+ */
+(function () {
+    "use strict"
+
+})()
